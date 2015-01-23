8a6d132b2c0a53a80557574b954314bad13962c0
psycholold <hannes.bibel@gmail.com>
2015-01-23 11:27:24 +0100

    Making display not touchable by setting flag, which is less awkward than the TouchBlockerView

--- app/src/main/java/com/abominableshrine/taptounlock/RecordPatternActivity.java
+++ app/src/main/java/com/abominableshrine/taptounlock/RecordPatternActivity.java
@@ -2,16 +2,26 @@ package com.abominableshrine.taptounlock;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
+/* The Activity from within a user can record a tap pattern */
 public class RecordPatternActivity extends Activity {
+    private Button mRecordPatternButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_pattern);
 
-        TouchBlockerView mTouchBlockerView = (TouchBlockerView) findViewById(R.id.touch_blocker_view);
-        mTouchBlockerView.disable_touch(false);
+        mRecordPatternButton = (Button) findViewById(R.id.record_pattern_button);
+        mRecordPatternButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+            }
+        });
     }
 
     // Note to self: Think about what happens when someone presses the back button while recording
